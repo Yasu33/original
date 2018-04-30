b3d9a7a3d633a2651e3425069a314410b0965ec7
Yasuko Namikawa <yasukonamikawa@yasukonomacbook-puro.local>
2018-04-30 11:09:43 +0900

    circle bound

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -7,18 +7,22 @@ void ofApp::setup(){
     music.setVolume(1);
     music.play();
     
+    ofSetFrameRate(60);
     ofEnableAlphaBlending();
     
-    circle_y = s_circle_y;
-
+    gravity=0.15;
+    friction=0.999;
     
     for (int i = 0; i<NUM; i++) {
-        s_circle_x[i] = ofRandom(100, ofGetWidth()-100);
-        circle_x[i] = s_circle_x[i];
-        change_x[i] = ofRandom(-4, 4);
+        circle_x[i] = ofRandom(20, ofGetWidth()-20); //円のx座標の初期値
+        circle_y[i] = ofGetHeight()/2; //円のy座標の初期値
+        speed_x[i] = ofRandom(-10, 10); //円のx軸方向のスピードの初期値
+        speed_y[i] = ofRandom(0,5); //円のy軸方向のスピードの初期値（自由落下）
         color_r[i] = ofRandom(120,255);
         color_g[i] = ofRandom(100,255);
         color_b[i] = ofRandom(100,255);
+        boundNum[i] = 0;
+
     }
     
     ofBackground(10, 10, 10);
@@ -29,30 +33,39 @@ void ofApp::update(){
     volume = ofSoundGetSpectrum(1);
     
     for(int i = 0; i<NUM ; i++){
+        //スピードの更新
+        speed_x[i] = speed_x[i] * friction;
+        speed_y[i] = speed_y[i] * friction;
+        speed_y[i] = speed_y[i] + gravity;
+        
+        //座標の更新
+        circle_x[i] = circle_x[i] + speed_x[i];
+        circle_y[i] = circle_y[i] + speed_y[i];
+        
+        //円のはね返り
         if(circle_x[i] < 0 || circle_x[i] > ofGetWidth()){
-            change_x[i] = -change_x[i];
+            speed_x[i] = -speed_x[i];
+        }
+        if (circle_y[i] >= ofGetHeight()) {
+            circle_y[i] = ofGetHeight();
+            speed_y[i] = -speed_y[i];
+            boundNum[i] = boundNum[i] + 1;
         }
         
-        circle_x[i]=circle_x[i] + change_x[i];
-    
     }
 }
 
 //--------------------------------------------------------------
 void ofApp::draw(){
-    ofTranslate(0, ofGetHeight());
     
-    ofPushMatrix();
-    ofRotateX(180);
-    
-        for (int i = 0; i<NUM; i++) {
-            ofSetColor(color_r[i], color_g[i], color_b[i], 180);
-            circle_r[i] = volume[0]*600;
-            ofDrawCircle(circle_x[i], circle_y, circle_r[i]);
-    
+    for (int i = 0; i<NUM; i++) {
+        if (boundNum[i] < 7) {
+            ofSetColor(color_r[i], color_g[i], color_b[i], 180-boundNum[i]*30); //円の色の設定
+            circle_r[i] = volume[0]*500; //円の半径の設定
+            ofDrawCircle(circle_x[i], circle_y[i], circle_r[i]); //円の描画
         }
-    
-    ofPopMatrix();
+    }
+
 }
 
 //--------------------------------------------------------------
--- src/ofApp.h
+++ src/ofApp.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "ofMain.h"
+
 #define NUM 50
 
 class ofApp : public ofBaseApp{
@@ -25,16 +26,18 @@ class ofApp : public ofBaseApp{
     ofSoundPlayer music;
     
     float *volume;
-    int circle_r[NUM];
-    int s_circle_x[NUM];
-    int s_circle_y = ofGetHeight()/2;
-    int circle_x[NUM];
-    int circle_y;
-    int change_x[NUM];
-    int change_y = 1;
+    float circle_r[NUM]; //円の半径
+    float circle_x[NUM]; //円のx座標
+    float circle_y[NUM]; //円のy座標
+    float speed_x[NUM]; //x軸方向のスピード
+    float speed_y[NUM]; //y軸方向のスピード
+    float gravity; //重力
+    float friction; //摩擦力
+    int boundNum[NUM]; //跳ね返った回数
     int color_r[NUM];
     int color_g[NUM];
     int color_b[NUM];
-    float time;
+    
+    ofEasyCam cam;
 		
 };
